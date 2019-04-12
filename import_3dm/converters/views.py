73323f62afd29d5bc9481b78b5f5cc98a128de94
Tom Svilans <tsvi@kadk.dk>
2019-04-12 12:03:58 +0200

    Updated license to 2019 and added Tom Svilans

--- import_3dm/converters/views.py
+++ import_3dm/converters/views.py
@@ -1,6 +1,6 @@
 # MIT License
 
-# Copyright (c) 2018 Nathan Letwory , Joel Putnam
+# Copyright (c) 2019 Nathan Letwory , Joel Putnam, Tom Svilans
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
