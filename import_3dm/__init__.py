13c9a4cf93378418cf79b93b85169f0a6536cb77
Tom Svilans <tom.svilans@gmail.com>
2019-11-05 15:35:13 +0100

    Added Lukas Fertig to bl_info.author field

--- import_3dm/__init__.py
+++ import_3dm/__init__.py
@@ -23,7 +23,7 @@
 
 bl_info = {
     "name": "Import Rhinoceros 3D",
-    "author": "Nathan 'jesterKing' Letwory, Joel Putnam, Tom Svilans",
+    "author": "Nathan 'jesterKing' Letwory, Joel Putnam, Tom Svilans, Lukas Fertig",
     "version": (0, 0, 5),
     "blender": (2, 80, 0),
     "location": "File > Import > Rhinoceros 3D (.3dm)",
