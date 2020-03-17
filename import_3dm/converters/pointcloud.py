ea588dc0c163c5de644d0933873c89f0a6575a38
Tom Svilans <tom.svilans@gmail.com>
2020-03-17 23:15:32 +0100

    Add support for Pointcloud objects (#68)
    * Fixes PolylineCurve name, fixes default Rhino material name, shows type of unsupported object
    
    * Adds basic support for Pointcloud objects
    
    * Small cleanup in pointcloud.py
    
    Co-authored-by: Tom Svilans <tsvi@kadk.dk>

---
+++ import_3dm/converters/pointcloud.py
@@ -0,0 +1,42 @@
+# MIT License
+
+# Copyright (c) 2018-2019 Nathan Letwory, Joel Putnam, Tom Svilans, Lukas Fertig 
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import rhino3dm as r3d
+from . import utils
+
+
+def import_pointcloud(context, ob, name, scale, options):
+    
+    og = ob.Geometry
+    oa = ob.Attributes
+
+    # add points as mesh vertices
+
+    # The following line crashes. Seems rhino3dm does not like iterating over pointclouds.
+    #vertices = [(p.X * scale, p.Y * scale, p.Z * scale) for p in og] 
+
+    vertices = [(og[v].X * scale, og[v].Y * scale, og[v].Z * scale) for v in range(og.Count)]
+    
+    pointcloud = context.blend_data.meshes.new(name=name)
+    pointcloud.from_pydata(vertices, [], [])
+    
+    return pointcloud
