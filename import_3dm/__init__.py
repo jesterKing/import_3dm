1adb214ab7bdbe49d8fee66543a1ec833c76fd59
Nathan Letwory <nathan@letworyinteractive.com>
2024-06-03 22:18:52 +0300

    bump version to 0.0.12 in prep for release.

--- import_3dm/__init__.py
+++ import_3dm/__init__.py
@@ -24,7 +24,7 @@
 bl_info = {
     "name": "Import Rhinoceros 3D",
     "author": "Nathan 'jesterKing' Letwory, Joel Putnam, Tom Svilans, Lukas Fertig",
-    "version": (0, 0, 11),
+    "version": (0, 0, 12),
     "blender": (3, 3, 0),
     "location": "File > Import > Rhinoceros 3D (.3dm)",
     "description": "This addon lets you import Rhinoceros 3dm files in Blender 3.3 and later",
